ca67c44b345e0654c45a33d15f7d3a90434ade52
Tamara Vasilevska <tamara.vasilevska@testdevlab.com>
2023-11-26 21:15:01 +0100

    adding login feature

--- features/support/pages.js
+++ features/support/pages.js
@@ -1,10 +1,12 @@
+import { LoginPage } from "../page-objects/loginPage";
 import { MainPage } from "../page-objects/mainPage";
 import { RegisterPage } from "../page-objects/registerPage";
 
 class Pages {
     constructor(){
         this.mainPage = new MainPage(),
-        this.registerPage = new RegisterPage()
+        this.registerPage = new RegisterPage(),
+        this.loginPage = new LoginPage()
     }
 }
 
--- features/support/tests.js
+++ features/support/tests.js
@@ -1,10 +1,12 @@
+import { LoginPageTest } from "../test-objects/loginPageTest";
 import { MainPageTest } from "../test-objects/mainPageTest";
 import { RegisterPageTest } from "../test-objects/registerPageTest";
 
 class Tests{
     constructor(){
        this.mainPageTest = new MainPageTest(),
-       this.registerPageTest = new RegisterPageTest()
+       this.registerPageTest = new RegisterPageTest(),
+       this.loginPageTest = new LoginPageTest()
     }
 }
 
